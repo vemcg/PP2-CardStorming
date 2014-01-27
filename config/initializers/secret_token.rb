261bfa93e8595c6a667e3a126ec767bdc1c999e1
Vern McGeorge <VernMcGeorge@gmail.com>
2014-01-26 17:17:43 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PP2Cardstorming::Application.config.secret_token = 'c397b4ff6ce0f8d7509038ae0dcebafb21cb040a7792caa224669b405be860fac24269a116b33768b465be77d76d48eb0f6296ab9e6f209ffcc351179c057637'
