4c0780187faf5c2463dc4ef5f6e3ca12c93a9f19
Glenn Holt <glennholt1@gmail.com>
2014-03-01 15:35:26 -0800

    deleted a lot of assets in the wrong locaction

--- app/assets/javascripts/vendor/OLDapp.js
+++
@@ -1,5 +0,0 @@
-require(['calculator/subtract', 'calculator/square'], function (sub, sq) {
-	console.log(sub(18,5));
-	
-	console.log(sq(11));
-});
--- public/calculator/square.js
+++
@@ -1,5 +0,0 @@
-define(['calculator/multiply'], function (multiply) {
-	return function (x) {
-		return multiply(x, x);
-	};
-});
